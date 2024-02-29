02d3c8fce1c00592d98981bb8daeef23cb62ea11
VSkoshchuk <skoschuk999@gmail.com>
2024-02-29 17:34:01 +0200

    SN-1313 Updating lifetime leads to generating registration update request that contains new lifetime for server

--- core/internals.h
+++ core/internals.h
@@ -100,7 +100,9 @@
 ((S) == STATE_REG_FAILED ? "STATE_REG_FAILED" :                 \
 ((S) == STATE_REG_UPDATE_PENDING ? "STATE_REG_UPDATE_PENDING" : \
 ((S) == STATE_REG_UPDATE_NEEDED ? "STATE_REG_UPDATE_NEEDED" :   \
-((S) == STATE_REG_FULL_UPDATE_NEEDED ? "STATE_REG_FULL_UPDATE_NEEDED" :   \
+((S) == STATE_REG_LT_UPDATE_NEEDED ? "STATE_REG_LT_UPDATE_NEEDED" :   \
+((S) == STATE_REG_OBJ_UPDATE_NEEDED ? "STATE_REG_OBJ_UPDATE_NEEDED" :   \
+((S) == STATE_REG_FULL_UPDATE_NEEDED ? "STATE_REG_FULL_UPDATE_NEEDED" : \
 ((S) == STATE_DEREG_PENDING ? "STATE_DEREG_PENDING" :           \
 ((S) == STATE_BS_HOLD_OFF ? "STATE_BS_HOLD_OFF" :               \
 ((S) == STATE_BS_INITIATED ? "STATE_BS_INITIATED" :             \
@@ -109,7 +111,7 @@
 ((S) == STATE_BS_FINISHING ? "STATE_BS_FINISHING" :             \
 ((S) == STATE_BS_FAILING ? "STATE_BS_FAILING" :                 \
 ((S) == STATE_BS_FAILED ? "STATE_BS_FAILED" :                   \
-"Unknown"))))))))))))))))
+"Unknown"))))))))))))))))))
 #define STR_MEDIA_TYPE(M)                                        \
 ((M) == LWM2M_CONTENT_TEXT ? "LWM2M_CONTENT_TEXT" :              \
 ((M) == LWM2M_CONTENT_LINK ? "LWM2M_CONTENT_LINK" :              \
