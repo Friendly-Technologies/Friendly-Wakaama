d88e9c01d31fbc5b386cef052b02a16b0982674a
anton <anton@sinai.io>
2024-03-13 18:14:30 +0200

    SN-1314 Primal set up the cbor.c for the library usage and check

--- data/cbor.c
+++ data/cbor.c
@@ -1,13 +1,11 @@
+#include "internals.h"
 #include <cbor.h>
-
-#ifdef LWM2M_SUPPORT_CBOR
-
 
 int cbor_parse(const uint8_t * buffer,
               size_t bufferLen,
               lwm2m_data_t ** dataP)
 {
-
+    return 0;
 }
 
 int cbor_serialize(bool isResourceInstance, 
@@ -15,7 +13,5 @@ int cbor_serialize(bool isResourceInstance,
                   lwm2m_data_t * dataP,
                   uint8_t ** bufferP)
 {
-
+    return 0;
 }
-
-#endif
