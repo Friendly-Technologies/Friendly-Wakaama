5de27f12c3d70c2002e51601a35cae05aff211fa
VSkoshchuk <skoschuk999@gmail.com>
2024-04-06 18:01:53 +0300

    SN-1322 Updated implementation of the object operation handling, created enum with supported operations and function for determining required one

--- core/objects/access_control.c
+++ core/objects/access_control.c
@@ -4,3 +4,8 @@
 
 #include "internals.h"
 #include <stdio.h>
+
+
+bool acl_is_operation_authorized(lwm2m_server_t * serverP, lwm2m_uri_t *uriP, lwm2m_obj_operation_t operation) {
+    return true;
+}
